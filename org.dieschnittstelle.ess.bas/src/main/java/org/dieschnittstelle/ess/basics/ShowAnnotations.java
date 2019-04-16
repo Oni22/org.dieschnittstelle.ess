4c678231d3c458f78bbc0a67a2b726c2fd4da3ae
RufusGladiuz <rouven.reuter91@gmail.com>
2019-04-16 18:48:30 +0200

    Added Annotations

--- org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
+++ org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
@@ -4,7 +4,12 @@ package org.dieschnittstelle.ess.basics;
 import org.dieschnittstelle.ess.basics.annotations.AnnotatedStockItemBuilder;
 import org.dieschnittstelle.ess.basics.annotations.StockItemProxyImpl;
 
+import java.lang.reflect.Field;
+import java.lang.annotation.*;
+import java.lang.reflect.Method;
+
 import static org.dieschnittstelle.ess.utils.Utils.*;
+
 
 public class ShowAnnotations {
 
@@ -27,10 +32,78 @@ public class ShowAnnotations {
 	}
 
 	/*
-	 * UE BAS2 
+	 * UE BAS2
 	 */
-	private static void showAttributes(Object consumable) {
-		show("class is: " + consumable.getClass());
+
+
+	private static void showAttributes(Object consumable)
+	{
+
+		Class klass = consumable.getClass();
+		String result = "{"+klass.getSimpleName();
+
+		for(Field field : klass.getDeclaredFields())
+		{
+			field.setAccessible(true);
+			result +=  " " +GetAttributeName(field) + ":";
+
+			try
+			{
+				result += field.get(consumable).toString() + ",";
+			}
+			catch (Exception e)
+			{
+				result += "Attribute does not match class";
+			}
+		}
+
+		char[] stringAsArray = result.toCharArray();
+		stringAsArray[stringAsArray.length -1] = '}';
+
+		result = String.valueOf(stringAsArray);
+
+		System.out.println(result);
 	}
 
+
+	private static String GetAttributeName(Field checkForAnnotation)
+	{
+		String outPut = " ";
+
+
+		Annotation[] annotations =  checkForAnnotation.getDeclaredAnnotations();
+
+
+			for(int i =0; i<= annotations.length-1;i++)
+			{
+
+
+				if(annotations[i].annotationType().toString().contains("DisplayAs"))
+				{
+						Class<? extends Annotation> type = annotations[i].annotationType();
+
+						for (Method method : type.getDeclaredMethods())
+						{
+							method.setAccessible(true);
+							try
+							{
+								Object value = method.invoke(annotations[i], (Object[]) null);
+								//System.out.println(" " + method.getName() + ": " + value);
+								return (String) value;
+							}
+							catch(Exception e)
+							{
+								return checkForAnnotation.getName();
+							}
+						}
+					}
+
+			}
+
+
+		return checkForAnnotation.getName();
+
+	}
 }
+
+
--- org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/annotations/stockitemtypes/Milch.java
+++ org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/annotations/stockitemtypes/Milch.java
@@ -5,7 +5,7 @@ import org.dieschnittstelle.ess.basics.annotations.Brandname;
 import org.dieschnittstelle.ess.basics.annotations.StockItem;
 import org.dieschnittstelle.ess.basics.annotations.Purchase;
 import org.dieschnittstelle.ess.basics.annotations.Units;
-
+import org.dieschnittstelle.ess.basics.annotations.DisplayAs;
 @StockItem
 public class Milch {
 
@@ -13,6 +13,9 @@ public class Milch {
 	private int menge;
 
 	@Brandname
+	@DisplayAs(
+			value = "Extra thick bundle of Milk"
+	)
 	private String markenname;
 
 	public int getMenge() {

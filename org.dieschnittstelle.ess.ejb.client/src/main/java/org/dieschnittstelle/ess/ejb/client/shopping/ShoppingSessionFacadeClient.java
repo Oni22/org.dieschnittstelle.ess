3415e9b6a930325393bfa32f49a2f7d39ac52add
Onur <onur-ozkan@hotmail.de>
2019-07-02 12:58:04 +0200

    pat 1 done

--- org.dieschnittstelle.ess.ejb.client/src/main/java/org/dieschnittstelle/ess/ejb/client/shopping/ShoppingSessionFacadeClient.java
+++ org.dieschnittstelle.ess.ejb.client/src/main/java/org/dieschnittstelle/ess/ejb/client/shopping/ShoppingSessionFacadeClient.java
@@ -1,14 +1,14 @@
 package org.dieschnittstelle.ess.ejb.client.shopping;
 
 import org.apache.logging.log4j.Logger;
+
 import org.dieschnittstelle.ess.ejb.client.ejbclients.EJBProxyFactory;
 import org.dieschnittstelle.ess.ejb.ejbmodule.crm.ShoppingException;
+import org.dieschnittstelle.ess.ejb.ejbmodule.crm.shopping.ShoppingSessionFacade;
 import org.dieschnittstelle.ess.ejb.ejbmodule.crm.shopping.ShoppingSessionFacadeRemote;
 import org.dieschnittstelle.ess.entities.crm.AbstractTouchpoint;
 import org.dieschnittstelle.ess.entities.crm.Customer;
 import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
-
-import javax.ejb.Stateful;
 
 public class ShoppingSessionFacadeClient implements ShoppingBusinessDelegate {
 
@@ -18,27 +18,30 @@ public class ShoppingSessionFacadeClient implements ShoppingBusinessDelegate {
 	/*
 	 * TODO use a proxy for the ShoppingSessionFacadeRemote interface
 	 */
-	private ShoppingSessionFacade ssf;
+	private ShoppingSessionFacadeRemote ssf;
 
 	public ShoppingSessionFacadeClient() {
 		/* TODO: instantiate the proxy using the EJBProxyFactory (see the other client classes) */
-		ssf = (ShoppingSessionFacade)EJBProxyFactory.getInstance().getProxy(ShoppingSessionFacade.class, "");
+		ssf = (ShoppingSessionFacadeRemote) EJBProxyFactory.getInstance().getProxy(ShoppingSessionFacadeRemote.class, "ejb:org.dieschnittstelle.ess.ejb/org.dieschnittstelle.ess.ejb.ejbmodule.crm/ShoppingSessionFacade!org.dieschnittstelle.ess.ejb.ejbmodule.crm.shopping.ShoppingSessionFacadeRemote?stateful");
 	}
 
 	/* TODO: implement the following methods using the proxy */
 
 	@Override
 	public void setTouchpoint(AbstractTouchpoint touchpoint) {
+
 		ssf.setTouchpoint(touchpoint);
 	}
 
 	@Override
 	public void setCustomer(Customer customer) {
+
 		ssf.setCustomer(customer);
 	}
 
 	@Override
 	public void addProduct(AbstractProduct product, int units) {
+
 		ssf.addProduct(product,units);
 	}
 
---
+++ org.dieschnittstelle.ess.ejb.ejbmodule.crm/src/main/java/org/dieschnittstelle/ess/ejb/ejbmodule/crm/shopping/ShoppingSessionFacade.java
@@ -0,0 +1,136 @@
+package org.dieschnittstelle.ess.ejb.ejbmodule.crm.shopping;
+
+import org.apache.logging.log4j.Logger;
+import org.dieschnittstelle.ess.ejb.ejbmodule.crm.*;
+import org.dieschnittstelle.ess.ejb.ejbmodule.crm.shopping.ShoppingSessionFacadeRemote;
+import org.dieschnittstelle.ess.entities.crm.AbstractTouchpoint;
+import org.dieschnittstelle.ess.entities.crm.Customer;
+import org.dieschnittstelle.ess.entities.crm.CustomerTransaction;
+import org.dieschnittstelle.ess.entities.crm.ShoppingCartItem;
+import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
+import org.dieschnittstelle.ess.entities.erp.Campaign;
+
+import javax.ejb.EJB;
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateful;
+import java.util.List;
+
+
+@Stateful
+@Remote(ShoppingSessionFacadeRemote.class)
+public class ShoppingSessionFacade implements ShoppingSessionFacadeRemote {
+
+    protected static Logger logger = org.apache.logging.log4j.LogManager.getLogger(ShoppingSessionFacade.class);
+
+    @EJB
+    private ShoppingCartLocal shoppingCart;
+
+    @EJB
+    private CustomerTrackingLocal customerTracking;
+
+    @EJB
+    private CampaignTrackingLocal campaignTracking;
+
+
+    /**
+     * the customer
+     */
+    private Customer customer;
+
+    /**
+     * the touchpoint
+     */
+    private AbstractTouchpoint touchpoint;
+
+
+    public void setTouchpoint(AbstractTouchpoint touchpoint) {
+        this.touchpoint = touchpoint;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void addProduct(AbstractProduct product, int units) {
+        this.shoppingCart.addItem(new ShoppingCartItem(product.getId(), units, product instanceof Campaign));
+    }
+
+    /*
+     * verify whether campaigns are still valid
+     */
+    public void verifyCampaigns() throws ShoppingException {
+        if (this.customer == null || this.touchpoint == null) {
+            throw new RuntimeException("cannot verify campaigns! No touchpoint has been set!");
+        }
+
+        for (ShoppingCartItem item : this.shoppingCart.getItems()) {
+            if (item.isCampaign()) {
+                int availableCampaigns = this.campaignTracking.existsValidCampaignExecutionAtTouchpoint(
+                        item.getErpProductId(), this.touchpoint);
+                logger.info("got available campaigns for product " + item.getErpProductId() + ": "
+                        + availableCampaigns);
+                // we check whether we have sufficient campaign items available
+                if (availableCampaigns < item.getUnits()) {
+                    throw new ShoppingException("verifyCampaigns() failed for productBundle " + item
+                            + " at touchpoint " + this.touchpoint + "! Need " + item.getUnits()
+                            + " instances of campaign, but only got: " + availableCampaigns);
+                }
+            }
+        }
+    }
+
+    public void purchase()  throws ShoppingException {
+        logger.info("purchase()");
+
+        if (this.customer == null || this.touchpoint == null) {
+            throw new RuntimeException(
+                    "cannot commit shopping session! Either customer or touchpoint has not been set: " + this.customer
+                            + "/" + this.touchpoint);
+        }
+
+        // verify the campaigns
+        verifyCampaigns();
+
+        // remove the products from stock
+        checkAndRemoveProductsFromStock();
+
+        // then we add a new customer transaction for the current purchase
+        List<ShoppingCartItem> products = this.shoppingCart.getItems();
+        CustomerTransaction transaction = new CustomerTransaction(this.customer, this.touchpoint, products);
+        transaction.setCompleted(true);
+        customerTracking.createTransaction(transaction);
+
+        logger.info("purchase(): done.\n");
+    }
+
+    /*
+     * to be implemented as server-side method for PAT2
+     */
+    private void checkAndRemoveProductsFromStock() {
+        logger.info("checkAndRemoveProductsFromStock");
+
+        for (ShoppingCartItem item : this.shoppingCart.getItems()) {
+
+            // TODO: ermitteln Sie das AbstractProduct für das gegebene ShoppingCartItem. Nutzen Sie dafür dessen erpProductId und die ProductCRUD EJB
+
+            if (item.isCampaign()) {
+                this.campaignTracking.purchaseCampaignAtTouchpoint(item.getErpProductId(), this.touchpoint,
+                        item.getUnits());
+                // TODO: wenn Sie eine Kampagne haben, muessen Sie hier
+                // 1) ueber die ProductBundle Objekte auf dem Campaign Objekt iterieren, und
+                // 2) fuer jedes ProductBundle das betreffende Produkt in der auf dem Bundle angegebenen Anzahl, multipliziert mit dem Wert von
+                // item.getUnits() aus dem Warenkorb,
+                // - hinsichtlich Verfuegbarkeit ueberpruefen, und
+                // - falls verfuegbar, aus dem Warenlager entfernen - nutzen Sie dafür die StockSystem EJB
+                // (Anm.: item.getUnits() gibt Ihnen Auskunft darüber, wie oft ein Produkt, im vorliegenden Fall eine Kampagne, im
+                // Warenkorb liegt)
+            } else {
+                // TODO: andernfalls (wenn keine Kampagne vorliegt) muessen Sie
+                // 1) das Produkt in der in item.getUnits() angegebenen Anzahl hinsichtlich Verfuegbarkeit ueberpruefen und
+                // 2) das Produkt, falls verfuegbar, in der entsprechenden Anzahl aus dem Warenlager entfernen
+            }
+
+        }
+    }
+}
